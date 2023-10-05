4cb00be2539bfb42f43b05aaa88683bfc04d5dd2
Eman Gurung <egurung1117@gmail.com>
2023-10-05 00:59:59 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -21,7 +21,36 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def find_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
 
+    if earthquake:
+        # Will return Earthquake JSON format
+        status = 200
+        return make_response(earthquake.to_dict(), status)
+    else:
+        # return message
+        status = 404
+        body = {
+            "message" : f"Earthquake {id} not found."
+        }
+        return make_response(body, status)
+    
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def min_magnitude(magnitude):
+    status = 200
+    earthquakes = []
+
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+
+    body = {
+        "count" : len(earthquakes),
+        "quakes" : earthquakes
+    }
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
